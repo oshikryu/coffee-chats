85b323c77de9e5bd12de805077f1525d9e32b9ae
Ryuta Oshikiri <ryushikiri@gmail.com>
2022-03-08 12:50:39 -0800

    Adding main rotato potato algorithm

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # coffee-chats
-Random matching every 2 weeks by email
+Randomly match emails and send out calendar invites for matches
+
+Generate your own creds as `credentials.json` from gcp
+
+## Run the code
+
+```
+yarn install
+yarn start
+```
---
+++ src/main.js
@@ -0,0 +1,177 @@
+import fs from 'fs'
+import csvParse from 'csv-parse'
+import readline from 'readline'
+import { google } from 'googleapis'
+import { DateTime } from 'luxon'
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+export const initMatcher = async (emails) => {
+  const params = {
+    emails,
+  };
+
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Gmail API.
+    authorize(JSON.parse(content), (auth) => generateMatches(auth, params));
+  });
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+const shuf = (arr) => {
+  for (let i=0; i<arr.length; i++) {
+    const rand = Math.floor(Math.random() * (arr.length - i) + i);
+    const temp = arr[i];    
+    arr[i] = arr[rand];
+    arr[rand] = temp;
+  }
+  return arr;
+}
+
+const hasMetMap = {}
+const initHasMetMap = (emails) => {
+  console.log(emails);
+  emails.forEach((email) => {
+    hasMetMap[email] = [];
+  })
+}
+
+/**
+ * Only works for even numbers of emails
+ *
+ * @method recursivelyGetMatches
+ * @return {Array}
+ */
+const getMatches = (shuffled) => {
+  const shuffleOne = shuffled.slice(0, shuffled.length / 2)
+  const shuffleTwo = shuffled.slice(shuffled.length / 2, shuffled.length)
+  let validMatches = true;
+
+  shuffleOne.forEach((email, idx) => {
+    const isSelf = email === shuffleTwo[idx];
+    const alreadyMet = hasMetMap[email].includes(shuffleTwo[idx])
+
+    if (isSelf || alreadyMet) {
+      validMatches = false
+    }
+  })
+
+  return validMatches ? { shuffleOne, shuffleTwo } : getMatches(shuffled);
+}
+
+const logMatch = (listOne, listTwo) => {
+  listOne.forEach((email, idx) => {
+    if (!hasMetMap[email].includes(listTwo[idx])) {
+      hasMetMap[email].push(listTwo[idx])
+      hasMetMap[listTwo[idx]].push(email)
+    }
+  });
+}
+
+async function generateMatches(auth, params) {
+  const { emails } = params
+  initHasMetMap(emails)
+
+  const shuffled = shuf(emails.slice());
+  const { shuffleOne, shuffleTwo } = getMatches(shuffled);
+
+  // add to already met list
+  logMatch(shuffleOne, shuffleTwo);
+
+  const listOne = shuffleOne.slice();
+  const listTwo = shuffleTwo.slice();
+
+  // rotate list two
+  for (let i=0; i < listTwo.length - 1; i++) {
+    listTwo.push(listTwo.shift());
+    logMatch(listOne, listTwo);
+    logMatch(shuffleTwo, listTwo);
+  }
+
+  // rotate list one
+  for (let i=0; i < listOne.length - 1; i++) {
+    listOne.push(listOne.shift());
+    logMatch(shuffleOne, listOne);
+  }
+
+  console.log(hasMetMap);
+}
+
+const processCsv = (input) => {
+  return input.map((input) => input[0] )
+}
+
+/**
+ *  read csv file
+ *  export to array of arrays
+ *
+ *  @param {String} input file
+ *  @return {Array}
+ */
+export const readCSV = (inputPath) => {
+  fs.readFile(inputPath, function (err, fileData) {
+    csvParse(fileData, {columns: false, trim: true}, function(err, rows, idx) {
+      const results = processCsv(rows.splice(1))
+      initMatcher(results);
+    })
+  })
+}
+
+const FILENAME = process.env.INPUT_FILE || 'input.csv'
+readCSV(FILENAME)
